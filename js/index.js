6feff021e22ddaa597da1fd89216b3e8cb7cccc7
Steven Pérez <Steven@Air-de-Steven.home>
2018-04-17 23:12:11 +0200

    initial commit

---
+++ js/index.js
@@ -0,0 +1,24 @@
+function Resume() {
+    document.getElementById("a").style.display='none'; 
+    document.getElementById("b").style.display='block';
+    document.getElementById("c").style.display='none'; 
+    document.getElementById("d").style.display='none'; 
+}
+function Aboutme(){
+    document.getElementById("a").style.display='block'; 
+    document.getElementById("b").style.display='none';
+    document.getElementById("c").style.display='none'; 
+    document.getElementById("d").style.display='none'; 
+}
+function Skills(){
+    document.getElementById("a").style.display='none'; 
+    document.getElementById("b").style.display='none';
+    document.getElementById("c").style.display='block'; 
+    document.getElementById("d").style.display='none'; 
+}
+function Contact(){
+    document.getElementById("a").style.display='none'; 
+    document.getElementById("b").style.display='none';
+    document.getElementById("c").style.display='none'; 
+    document.getElementById("d").style.display='block'; 
+}
